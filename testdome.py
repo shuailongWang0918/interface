083e5262d5b12f6727eeb9ee88f0475c1eb958da
王帅龙 <wwangshuailong@gmail.com>
2018-12-04 16:08:29 +0800

    修改函数名

--- testdome.py
+++ testdome.py
@@ -1,7 +1,7 @@
 from Event.client import *
 
 url = "http://0.0.0.0"
-client = Client(url=url, method="post", body_type=body_type.URL_ENCODE)
+client = Client(url=url, method="post", body_type=BodyType.URL_ENCODE)
 client.set_body({})
 client.send()
 print(client.status_code)
