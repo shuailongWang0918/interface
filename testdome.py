26b7dff706d50208682798696602ae77fb231c85
王帅龙 <wwangshuailong@gmail.com>
2018-12-10 10:08:34 +0800

    Delete testdome.py

--- testdome.py
+++
@@ -1,9 +0,0 @@
-from Event.client import *
-
-url = "http://0.0.0.0"
-client = Client(url=url, method="post", body_type=BodyType.URL_ENCODE)
-client.set_body({})
-client.send()
-print(client.status_code)
-print(client.res_time)
-print(client.res_text)
